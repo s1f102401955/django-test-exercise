a38af1f47a2716c7b797a9f31635579bd3d2595a
1f10240195 <s1f102401955@iniad.org>
2025-07-17 13:38:23 +0900

    コードの修正

--- todo/tests.py
+++ todo/tests.py
@@ -127,7 +127,7 @@ class TodoViewTestCase(TestCase):
         response = client.get('/1/delete')
 
         self.assertEqual(response.status_code, 404)
-    
+
     def test_update_get_success(self):
         task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task.save()
